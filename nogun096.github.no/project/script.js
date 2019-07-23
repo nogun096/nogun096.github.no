aaa403bda58e974af62f27b9978d752f430bc552
nogun096 <50713964+nogun096@users.noreply.github.com>
2019-07-23 20:39:19 +0100

    Add files via upload

--- nogun096.github.no/project/script.js
+++ nogun096.github.no/project/script.js
@@ -1,3 +1,4 @@
+
 function is_url(str)
 {
   regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
@@ -7,8 +8,9 @@ function is_url(str)
         }
         else
         {
+
           return false;
         }
 }
  urls = $$('a'); for (url in urls) console.log (is_url( urls[url].href ));
-console.log(is_url("https://www.canada.ca/en/public-health/services/chronic-diseases/osteoporosis.html"));
+
